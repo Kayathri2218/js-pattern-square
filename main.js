4b48df80668c0f959b0455dfc22bfeda6e8fa133
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-28 17:06:44 +0530

    pattern in square.

---
+++ main.js
@@ -0,0 +1,8 @@
+//  square box in * print pattern 
+
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 5; j++) {
+                document.write("*" + " ");
+            }
+            document.write("<br>");
+        }
